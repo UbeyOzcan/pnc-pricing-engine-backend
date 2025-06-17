ca2daa5cee73b8a50ee4bd1c2d0e8249b551cef1
Ubey <ozcanubey@outlook.com>
2025-06-17 15:07:35 +0200

    add new RFs to make the premium more volatile

--- build_models.py
+++ build_models.py
@@ -4,15 +4,17 @@ from src.DataFactory import DataFactory
 
 def main():
     DF = DataFactory()
-    MF = ModelFactory(df=DF.getData().head(10000)) # train model on 10k data just to make our life easier on deployment as this is a demo model =)
-    MF.frequency(expr="ClaimNb ~  Area + VehGas + VehAge + DrivAge")
-    MF.severity(expr="ClaimAmount ~ VehPower + VehAge + DrivAge +  VehGas + Area")
-    single_profile = {'VehPower': [10],
+    MF = ModelFactory(df=DF.getData()) # train model on 10k data just to make our life easier on deployment as this is a demo model =)
+    MF.frequency(expr="ClaimNb ~  Area + VehGas + VehAge + DrivAge + VehBrand + BonusMalus + Density")
+    MF.severity(expr="ClaimAmount ~ Area + VehGas + VehAge + DrivAge + VehBrand + BonusMalus + Density")
+    single_profile = {'VehPower': [50],
                       'VehAge': [0],
-                      'DrivAge': [18],
-                      #'BonusMalus': [50],
+                      'DrivAge': [50],
+                      'BonusMalus': [90],
                       'VehGas': ['Regular'],
-                      'Area': ['C']}
+                      'Area': ['C'],
+                      'Density': [2000],
+                      'VehBrand': ['B12']}
     rp = MF.riskpremium(env='prod', single_profile=single_profile)
     print(rp)
 
